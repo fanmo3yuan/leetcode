b9782328f42c23aaea998225b195fdde157dc157
fanmo.zjj <fanmo.zjj@alibaba-inc.com>
2020-05-16 23:47:05 +0800

    GroupAnagrams

---
+++ src/main/java/fanmo/leetcode/algorithms/GroupAnagrams.java
@@ -0,0 +1,39 @@
+package fanmo.leetcode.algorithms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * {@see https://leetcode-cn.com/problems/group-anagrams/}
+ *
+ * @author fanmo
+ */
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> results = new ArrayList<>();
+        if (null == strs || 0 == strs.length) {
+            return results;
+        }
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] ca = str.toCharArray();
+            Arrays.sort(ca);
+            String sortedStr = Arrays.toString(ca);
+            if (!map.containsKey(sortedStr)){
+                map.put(sortedStr, new ArrayList<>());
+            }
+            map.get(sortedStr).add(str);
+        }
+        results.addAll(map.values());
+        return results;
+    }
+
+    public static void main(String[] args) {
+        GroupAnagrams ga = new GroupAnagrams();
+    }
+
+}
